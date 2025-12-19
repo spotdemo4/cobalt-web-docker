81bcb285dd5c151a8b7104ed7b0c5b23493c9a81
spotdemo4 <me@trev.xyz>
2025-12-19 05:56:28 -0500

    fix: exit instead of return

--- check.sh
+++ check.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 
-sudo apt-get -y install skopeo
-
 cobalt_version=$(curl -s https://raw.githubusercontent.com/imputnet/cobalt/refs/heads/main/web/package.json | jq -r '.version')
 if [[ -z "$cobalt_version" ]]; then
     echo "failed to fetch the latest version"
-    return 1
+    exit 1
 fi
 echo "latest cobalt web version: $cobalt_version"
 
@@ -15,14 +13,14 @@ else
     readarray -t tags < <(skopeo list-tags --creds "trev:${token:-}" "docker://${registry:-}" | jq -r '.Tags[]')
     if [[ ${#tags[@]} -eq 0 ]]; then
         echo "failed to fetch existing tags from ghcr.io"
-        return 1
+        exit 1
     fi
     echo "current tags: ${tags[*]}"
 
     for tag in "${tags[@]}"; do
         if [[ "$cobalt_version" == "$tag" ]]; then
             echo "tag $tag already exists, skipping update"
-            return 0
+            exit 0
         fi
     done
 
